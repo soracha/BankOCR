88522f40a80ce85c02227eb6d3d2b3e585f637b3
Soracha Thepsatitsilp <fai@abctech-thailand.com>
2012-06-22 10:36:21 +0700

    modify tranform_num() for '222222222'

--- BankOCR.pm
+++ BankOCR.pm
@@ -14,12 +14,21 @@ sub tranform_num {
 			   "  |  |  |  |  |  |  |  |  |" .
 			   "                           " ;
 
+	my $str2 = " _  _  _  _  _  _  _  _  _ " .
+			   " _| _| _| _| _| _| _| _| _|" .
+			   "|_ |_ |_ |_ |_ |_ |_ |_ |_ " .
+			   "                           " ;
+
 	if ( $input eq $str0 ) {
 		return "000000000" ;
 	}
 
 	elsif ( $input eq $str1 ) {
 		return "111111111" ;
+	}
+
+	elsif ( $input eq $str2 ) {
+		return "222222222" ;
 	}
 
 	return "Not number" ;
